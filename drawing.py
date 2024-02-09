c4704b854ce716a7943d2d01615fbd0b4821f6e6
borkmaster0 <149451125+borkmaster0@users.noreply.github.com>
2024-02-09 11:44:42 -0500

    Create drawing & main

---
+++ drawing.py
@@ -0,0 +1,11 @@
+from turtle import *
+
+def createTurtle(name):
+    name = Turtle()
+
+def line(x: float, y: float, x1: float, y1: float):
+    return [[x, y], [x1, y1]]
+
+def point(x: float, y: float):
+    return [x, y]
+
